d0965e5da64a23ba552c7f88af4688fd9ce3c6db
unknown <edward.cruzn@gmail.com>
2024-10-18 23:05:20 -0500

    fix: se actualiza los procedimientos de la tabla horarios de veterinario

--- Script_tbl_horarios_veterinario.sql
+++ Script_tbl_horarios_veterinario.sql
@@ -1,17 +1,19 @@
 -- Insertar un nuevo horario de veterinario
 DELIMITER //
 CREATE PROCEDURE spInsertVeterinaryHours(
-	IN p_date DATE,
+	IN p_start_date DATE,
+    IN p_end_date DATE,
 	IN p_start_time TIME,
 	IN p_final_time TIME,
 	IN p_fkveterinarian INT)
 BEGIN
 	INSERT INTO tbl_horarios_veterinario(
-	hor_vet_fecha, 
+	hor_vet_fecha_inicio, 
+    hor_vet_fecha_final,
 	hor_vet_hora_inicio, 
 	hor_vet_hora_final, 
 	tbl_veterinario_vet_id)
-    VALUES(p_date, p_start_time, p_final_time, p_fkveterinarian);
+    VALUES(p_start_date, p_end_date, p_start_time, p_final_time, p_fkveterinarian);
 END//
 DELIMITER ;
 
@@ -20,14 +22,16 @@ DELIMITER ;
 DELIMITER //
 CREATE PROCEDURE spUpdateVeterinaryHours(
 		IN p_hor_vet_id INT,
-		IN p_date DATE,
+		IN p_start_date DATE,
+        IN p_end_date DATE,
 		IN p_start_time TIME,
 		IN p_final_time TIME,
 		IN p_fkveterinarian INT)
 BEGIN
 	UPDATE tbl_horarios_veterinario
 	SET 
-		hor_vet_fecha = p_date, 
+		hor_vet_fecha_inicio = p_start_date,
+        hor_vet_fecha_final = p_end_date,
 		hor_vet_hora_inicio = p_start_time, 
 		hor_vet_hora_final = p_final_time, 
 		tbl_veterinario_vet_id = p_fkveterinarian
@@ -42,6 +46,8 @@ CREATE PROCEDURE spSelectVeterinaryHours()
 BEGIN
 	SELECT
 		hor_vet_id,
+        hor_vet_fecha_inicio,
+        hor_vet_fecha_final,
 		hor_vet_hora_inicio, 
 		hor_vet_hora_final, 
 		tbl_veterinario_vet_id, tbl_veterinario.vet_nombre
